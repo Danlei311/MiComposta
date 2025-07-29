ba2ad3859dde3fb6b0ef4a21b8e84844efc994ef
IDGS-904-22001661 <armandodanielr29@outlook.com>
2025-07-29 01:38:09 -0600

    correciones y moduloCompras Terminado

--- src/app/services/compras-servicio.ts
+++ src/app/services/compras-servicio.ts
@@ -20,4 +20,19 @@ export class ComprasServicio {
   getMaterialesDeProveedor(idProveedor: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/AdminProveedores/getMateriales/${idProveedor}`);
   }
+
+  // Método para realizar una compra
+  comprarMaterial(request: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/AdminCompras/comprarMaterial`, request);
+  }
+
+  // Obtener todas las compras
+  getCompras(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/AdminCompras/getCompras`);
+  }
+
+  // Obtener el detalle de una compra
+  getDetalleCompra(idCompra: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/AdminCompras/getDetalleCompra/${idCompra}`);
+  }
 }
