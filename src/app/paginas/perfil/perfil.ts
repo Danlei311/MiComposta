fa6dce061fac6f53c3a796b22c09d5528d6a36ae
IDGS-904-22001033 <alberto.rt@outlook.com>
2025-07-30 21:23:56 -0600

    Inicio de desarrollo de perfil de usuario

--- src/app/paginas/perfil/perfil.ts
+++ src/app/paginas/perfil/perfil.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { DatosUsuario } from './datos-usuario/datos-usuario';
+import { SeguridadUsuario } from './seguridad-usuario/seguridad-usuario';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [DatosUsuario, SeguridadUsuario, CommonModule, FormsModule],
   templateUrl: './perfil.html',
   styleUrl: './perfil.css'
 })
 export class Perfil {
+activeTab: 'perfil' | 'seguridad' = 'perfil';
 
+  switchTab(tab: 'perfil' | 'seguridad') {
+    this.activeTab = tab;
+  }
 }
---
+++ src/app/paginas/perfil/seguridad-usuario/seguridad-usuario.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seguridad-usuario',
+  imports: [],
+  templateUrl: './seguridad-usuario.html',
+  styleUrl: './seguridad-usuario.css'
+})
+export class SeguridadUsuario {
+
+}
